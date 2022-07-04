1e3f8d0d4b0df9823db7c83dc4ce778dc097375a
Juan Pablo Zaldumbide Proaño <jzaldumbide@gmail.com>
2022-07-04 09:46:43 -0500

    Initial commit

---
+++ src/principal.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class principal {
+    public static void main(String[] args) {
+        System.out.println("Hola a todos:");
+        System.out.println("Juan Pablo Zaldumbide");
+    }
+}
